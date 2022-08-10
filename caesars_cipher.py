da6802139ac281e041a59a4f8fd4ab27d07f412c
altersaber26 <altersaber26@gmail.com>
2022-08-10 16:56:21 +0300

    Added comments and language argument validation

--- caesars_cipher.py
+++ caesars_cipher.py
@@ -1,16 +1,25 @@
-import string
-from typing import Optional
 
-def cipher(message: str, /, *, shift: int, language: Optional[str] = "en") -> str:
+
+def cipher(message: str, /, *, shift: int, language: str) -> str:
+    # Validating "message" argument
     if type(shift) != int or shift < 1:
         raise ValueError("\"shift\" argument must be a positive integer")
+    # Validating "shift" argument
     if type(message) != str or not any([char.isalpha() for char in message]):
         raise ValueError("\"message\" argument must be a string with at least 1 alphabetic character")
+    # Validating "language" argument
     if language == "en":
-        alphabet = zip(string.ascii_lowercase, string.ascii_uppercase)
+        alphabet = "abcdefghijklmnopqrstuvwxyz"
+    elif language == "ru":
+        alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
+    else:
+        raise ValueError("\"language\" argument must be either \"en\" or \"ru\", other are not supported")
 
-    shift = shift % 26
+    # Preparing arguments for use
+    alphabet = zip(alphabet, alphabet.upper())
+    shift = shift % len(alphabet)
 
+    # Algorithm itself
     for char in message:
         if not char.isalpha():
             continue
